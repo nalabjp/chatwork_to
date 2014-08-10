e605b10ae5511f0c214fce58ecb8f9e8d3ba743a
nalabjp <nalabjp@gmail.com>
2014-08-11 04:16:15 +0900

    Implemented ChatworkTo::Response

---
+++ lib/chatwork_to/response.rb
@@ -0,0 +1,59 @@
+module ChatworkTo
+  class Response
+    def initialize(room_ids)
+      @responses = {}
+      room_ids.each do |room_id|
+        @responses[room_id.to_s] = default_response
+      end
+    end
+
+    def get(room_id)
+      @responses[room_id.to_s]
+    end
+
+    def set(room_id, response)
+      old = @responses[room_id.to_s]
+
+      current = default_response.merge({ 'success' => response['status']['success'] })
+      if current['success']
+        current['chat_list'] = response['result']['chat_list']
+        if old['last_chat_id'].eql?(0)
+          # init_load
+          if current['chat_list'].present?
+            current['last_chat_id'] = current['chat_list'].last.fetch('id')
+          end
+        else
+          if current['chat_list'].present?
+            # updated
+            current['last_chat_id'] = current['chat_list'].last.fetch('id')
+            current['notify'] = true
+          else
+            # not updated
+            current['last_chat_id'] = old['last_chat_id']
+          end
+        end
+      else
+        current['error_message'] = response['status']['message']
+      end
+
+      @responses[room_id.to_s] = current
+    end
+
+    def success?(room_id)
+      @responses[room_id.to_s]['success']
+    end
+
+    def notify?(room_id)
+      @responses[room_id.to_s]['notify']
+    end
+
+  private
+    def default_response
+      {
+        'success' => false,
+        'notify'  => false,
+        'last_chat_id => 0,
+      }
+    end
+  end
+end
